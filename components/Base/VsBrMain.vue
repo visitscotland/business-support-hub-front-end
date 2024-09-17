8b6075a6c0781c83ceec5bc760aea417485293ff
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-09-17 09:06:53 +0100

    fix(vsbrmain.vue): add meta data

--- components/Base/VsBrMain.vue
+++ components/Base/VsBrMain.vue
@@ -123,6 +123,21 @@ if (page.value) {
     const runtimeConfig = useRuntimeConfig();
 
     useHead({
+        title: document.model.data.seoTitle,
+        meta: [
+            {
+                name: 'title',
+                content: document.model.data.seoTitle,
+            },
+            {
+                name: 'description',
+                content: document.model.data.seoDescription,
+            },
+            {
+                name: 'robots',
+                content: document.model.data.noIndex ? 'noindex': '', 
+            },
+        ],
         htmlAttrs: {
             lang: langString,
             'data-version': configStore.pageMetaData.version,
