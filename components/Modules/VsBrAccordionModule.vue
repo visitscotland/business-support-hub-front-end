402dcf67436ebc9bb71baf5e022c9e26fa1067cc
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-10-10 12:45:54 +0100

    feat(vsbraccordionmodule): add media to accordion item content

--- components/Modules/VsBrAccordionModule.vue
+++ components/Modules/VsBrAccordionModule.vue
@@ -21,9 +21,26 @@
                 </template>
 
                 <div class="p-3">
-                    <VsBrRichText
-                        :input-content="item.content"
-                        class="p-3"
+                    <VsBrRichText :input-content="item.content" />
+
+                    <VsBrImageWithCaption
+                        v-if="item.image"
+                        :image="item.image.cmsImage"
+                    />
+                    <VsVideo
+                        v-if="item.video"
+                        :video-title="item.video.label"
+                        :video-id="item.video.youtubeId
+                            ? item.video.youtubeId
+                            : extractYoutubeId(item.video.url)
+                        "
+                        :locale="configStore.locale"
+                        :single-minute-descriptor="configStore.getLabel('video', 'video.minute-text')"
+                        :plural-minute-descriptor="configStore.getLabel('video', 'video.minutes-text')"
+                        :no-cookies-message="configStore.getLabel('video', 'video.no-cookies')"
+                        :no-js-message="configStore.getLabel('video', 'video.no-js')"
+                        :cookie-btn-text="configStore.getLabel('essentials.global', 'cookie.link-message')"
+                        :error-message="configStore.getLabel('essentials.global', 'third-party-error')"
                     />
                 </div>
             </VsAccordionItem>
@@ -40,9 +57,13 @@ import {
     VsContainer,
     VsHeading,
     VsRow,
+    VsVideo,
 } from '@visitscotland/component-library/components';
+import VsBrImageWithCaption from '~/components/Modules/VsBrImageWithCaption.vue';
 import VsBrRichText from '~/components/Modules/VsBrRichText.vue';
 import separateTitleFromContent from '~/composables/separateTitleFromContent';
+import useConfigStore from '~/stores/configStore';
+const configStore = useConfigStore();
 
 const props = defineProps<{
     module: LooseObject,
@@ -50,8 +71,14 @@ const props = defineProps<{
 
 // Set the accordionItem content by extracting the data from the sections.
 const accordionItems = computed(() => {
-    return props.module.sections.map(({ copy }: { copy: LooseObject }) => {
-        return separateTitleFromContent(copy.value);
+    return props.module.sections.map(({ copy, image, video, }: { copy: LooseObject, image: LooseObject, video: LooseObject }) => {
+        const { title, content } = separateTitleFromContent(copy.value)
+        return {
+            title,
+            content,
+            image,
+            video,
+        };
     });
 });
 </script>
