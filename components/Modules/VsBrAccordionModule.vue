c5475812b66a91e00fbce25fa8ced118b73bddd5
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-10-29 17:01:11 +0000

    feat(accordion module): change heading level when module is nested

--- components/Modules/VsBrAccordionModule.vue
+++ components/Modules/VsBrAccordionModule.vue
@@ -1,32 +1,32 @@
 <template>
-    <VsContainer>
-        <VsRow>
-            <VsCol>
-                <VsHeading
-                    :id="props.module.anchor ? props.module.anchor : null"
-                    level="2"
-                    headingStyle="heading-m"
+    <pre>{{ module }}</pre>
+    <VsModuleWrapper
+        :anchor-id="props.module.anchor || null"
+        :heading-level="isNested ? 3 : 2"
+        businessSupport
+    >
+        <template #vs-module-wrapper-heading>
+            {{ props.module.title }}
+        </template>
+        <VsContainer>
+            <VsAccordion>
+                <VsAccordionItem
+                    v-for="(item, index) in props.module.sections"
+                    :control-id="`${props.idPrefix}${index}`"
+                    :heading-level="isNested ? 4 : 3"
+                    variant="transparent"
                 >
-                    {{ props.module.title }}
-                </VsHeading>
-            </VsCol>
-        </VsRow>
-        <VsAccordion>
-            <VsAccordionItem
-                v-for="(item, index) in props.module.sections"
-                :control-id="`${props.idPrefix}${index}`"
-                variant="transparent"
-            >
-                <template #title>
-                    {{ item.heading }}
-                </template>
+                    <template #title>
+                        {{ item.heading }}
+                    </template>
 
-                <div class="p-075">
-                    <VsBrRichText :input-content="item.copy.value" />
-                </div>
-            </VsAccordionItem>
-        </VsAccordion>
-    </VsContainer>
+                    <div class="p-075">
+                        <VsBrRichText :input-content="item.copy.value" />
+                    </div>
+                </VsAccordionItem>
+            </VsAccordion>
+        </VsContainer>
+    </VsModuleWrapper>
 </template>
 
 <script setup lang="ts">
@@ -34,10 +34,8 @@ import type { LooseObject } from '~/types/types';
 import {
     VsAccordion,
     VsAccordionItem,
-    VsCol,
     VsContainer,
-    VsHeading,
-    VsRow,
+    VsModuleWrapper,
 } from '@visitscotland/component-library/components';
 import VsBrRichText from '~/components/Modules/VsBrRichText.vue';
 
@@ -45,4 +43,6 @@ const props = defineProps<{
     module: LooseObject,
     idPrefix: string,
 }>();
+
+const isNested = computed(() => props.module.nested);
 </script>
