53f41ebf29ce0302e4fc02acc33047c37f73d6e4
chesterroberts-vs <chester.roberts@visitscotland.com>
2024-11-20 14:32:48 +0000

    feat(newsletter): added newsletter cta at bottom of pages + added illustration

--- components/PageTypes/VsBrGeneral.vue
+++ components/PageTypes/VsBrGeneral.vue
@@ -11,6 +11,16 @@
         v-if="pageItems"
         :modules="pageItems"
     />
+
+    <NuxtLazyHydrate
+        :when-visible="{ rootMargin: '50px' }"
+    >
+    <pre>{{ documentData }}</pre>
+        <VsBrNewsletterSignpost 
+            v-if="!documentData.hideNewsletter && configStore.newsletterSignpost"
+            :data="configStore.newsletterSignpost"
+        />
+    </NuxtLazyHydrate>
 </template>
 
 <script lang="ts" setup>
@@ -22,6 +32,7 @@ import useConfigStore from '~/stores/configStore';
 import VsBrHeroSectionModule from '~/components/Modules/VsBrHeroSectionModule.vue';
 import VsBrPageIntro from '~/components/Modules/VsBrPageIntro.vue';
 import VsBrModuleBuilder from '~/components/Modules/VsBrModuleBuilder.vue';
+import VsBrNewsletterSignpost from '../Modules/VsBrNewsletterSignpost.vue';
 
 const props = defineProps<{
     component: Component,
