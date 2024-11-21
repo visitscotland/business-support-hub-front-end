14949494ceffbf69fd53eb7ea26b88ae3ab36e93
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-11-21 15:18:41 +0000

    feat(related links): add badges and tidy up code

--- components/Modules/VsBrRelatedLinks.vue
+++ components/Modules/VsBrRelatedLinks.vue
@@ -13,8 +13,6 @@
                     <VsStretchedLinkCard
                         :link="link.url"
                         type="internal"
-                        :img-src="link.imageSrc"
-                        :img-alt="link.imageAlt"
                     >
                         <template #stretched-card-header>
                             {{ link.title }}
@@ -23,19 +21,35 @@
                         <template #stretched-card-content>
                             {{ link.teaser }}
                         </template>
-                    </VsStretchedLinkCard>
 
-                    
+                        <template
+                            v-if="link.readTime || link.type"
+                            v-slot:stretched-card-badges
+                        >
+                            <div class="text-start">
+                                <VsBadge v-if="link.readTime">
+                                    {{ link.readTime }}
+                                </VsBadge>
+
+                                <VsBadge v-if="link.type">
+                                    {{ link.type }}
+                                </VsBadge>
+                            </div>
+                        </template>
+                    </VsStretchedLinkCard>
                 </VsCol>
             </VsRow>
         </VsContainer>
     </VsModuleWrapper>
+    <!-- TODO: Remove before PR -->
     <pre>{{ allContent }}</pre>
+    <!-- TODO: Remove before PR -->
 </template>
 
 <script setup lang="ts">
 import type { Page } from '@bloomreach/spa-sdk';
 import {
+    VsBadge,
     VsCol,
     VsContainer,
     VsModuleWrapper,
@@ -46,31 +60,39 @@ import {
 const props = defineProps<{
     relatedLinks: any[],
 }>();
+const page: Page | undefined = inject('page');
 
+// Temporary to see all available related link content.
 const allContent = ref<any[]>([]);
 
-const page: Page | undefined = inject('page');
 
 const relatedLinksContent = computed(() => {
     return props.relatedLinks.map((link) => {
         const content = page?.getContent(link.$ref);
+        const contentModel = content?.model;
+
         allContent.value.push(content);
-        const title = content.model.data.title;
-        const teaser = content.model.data.teaser;
-        const url = content.model.links.site.href;
-        const imageValue = page.getContent(content.model.data.image.$ref);
-        const imageSrc = imageValue.getOriginal().getUrl();
-        const imageAlt = imageValue.altText;
 
-        return { title, teaser, url, imageSrc, imageAlt };
-
-        // return {
-        //     title: content.model.data.title,
-        //     teaser: content.model.data.teaser,
-        //     url: content.model.links.site.href,
-        //     imageSrc: imageValue.getOriginal().getUrl(),
-        //     imageAlt: imageValue.altText,
-        // };
+        return {
+            readTime: displayReadTime(contentModel.data.readingTime),
+            teaser: contentModel.data.teaser,
+            title: contentModel.data.title,
+            type: captialiseFirstCharacter(contentModel.data.type),
+            url: contentModel.links.site.href,
+        };
     });
 });
+
+const captialiseFirstCharacter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLocaleLowerCase();
+};
+
+const displayReadTime = (time: number) => {
+    if (time === 0) return;
+
+    // TODO: replace hardcoded text with CMS labels.
+    const unit = (time > 1) ? 'minutes' : 'minute';
+
+    return `Reading time: ${time} ${unit}`;
+};
 </script>
