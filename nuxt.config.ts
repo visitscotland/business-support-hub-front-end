a9b913bb724c6861311aa46261e67d366d2a39e2
Mark Mitchell <7934920+markup-mitchell@users.noreply.github.com>
2025-01-17 12:09:30 +0000

    style(nuxt.config.ts): indentation and single quotes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,64 +13,64 @@ const clVersion = bufferFile('.clversion');
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  scripts: {
-      globals: {
-          search: 'https://customer.cludo.com/scripts/bundles/search-script.min.js',
-          results: '/scripts/cludo-search-results.js',
-      },
-  },
+    scripts: {
+        globals: {
+            search: 'https://customer.cludo.com/scripts/bundles/search-script.min.js',
+            results: '/scripts/cludo-search-results.js',
+        },
+    },
 
-  runtimeConfig: {
-      BR_RESOURCE_API_ENDPOINT: process.env.BR_RESOURCE_API_ENDPOINT,
-      BR_X_FORWARDED_HOST: process.env.BR_X_FORWARDED_HOST,
-      public: {
-          BR_CMS_ORIGIN_LOCATION: process.env.BR_CMS_ORIGIN_LOCATION,
-          BR_NUXT_APP_DEBUG: process.env.BR_NUXT_APP_DEBUG,
-          COMP_LIBRARY_VERSION: clVersion,
-      },
-  },
+    runtimeConfig: {
+        BR_RESOURCE_API_ENDPOINT: process.env.BR_RESOURCE_API_ENDPOINT,
+        BR_X_FORWARDED_HOST: process.env.BR_X_FORWARDED_HOST,
+        public: {
+            BR_CMS_ORIGIN_LOCATION: process.env.BR_CMS_ORIGIN_LOCATION,
+            BR_NUXT_APP_DEBUG: process.env.BR_NUXT_APP_DEBUG,
+            COMP_LIBRARY_VERSION: clVersion,
+        },
+    },
 
-  vite: {
-      resolve: {
-          preserveSymlinks: true,
-      },
-      build: {
-          cssCodeSplit: true,
-      },
-  },
+    vite: {
+        resolve: {
+            preserveSymlinks: true,
+        },
+        build: {
+            cssCodeSplit: true,
+        },
+    },
 
-  vue: {
-      runtimeCompiler: true,
-  },
+    vue: {
+        runtimeCompiler: true,
+    },
 
-  components: [
-      {
-          path: '~/components',
-          pathPrefix: false,
-      },
-  ],
+    components: [
+        {
+            path: '~/components',
+            pathPrefix: false,
+        },
+    ],
 
-  buildModules: [
-      '@nuxtjs/dotenv',
-  ],
+    buildModules: ['@nuxtjs/dotenv'],
 
-  modules: ['@pinia/nuxt', 'nuxt-jsonld', 'nuxt-lazy-hydrate', '@nuxt/scripts'],
+    modules: [
+        '@pinia/nuxt',
+        'nuxt-jsonld',
+        'nuxt-lazy-hydrate',
+        '@nuxt/scripts',
+    ],
 
-  'nuxt-jsonld': {
-      disableOptionsAPI: true,
-  },
+    'nuxt-jsonld': {
+        disableOptionsAPI: true,
+    },
 
-  css: [
-      '@visitscotland/component-library/components/style.css',
-      '@visitscotland/component-library/components/fonts.css',
-  ],
+    css: [
+        '@visitscotland/component-library/components/style.css',
+        '@visitscotland/component-library/components/fonts.css',
+    ],
 
-  build: {
-      transpile: [
-          'bootstrap-vue-next',
-          '@visitscotland/component-library',
-      ],
-  },
+    build: {
+        transpile: ['bootstrap-vue-next', '@visitscotland/component-library'],
+    },
 
-  compatibilityDate: '2024-12-10'
+    compatibilityDate: '2024-12-10',
 });
