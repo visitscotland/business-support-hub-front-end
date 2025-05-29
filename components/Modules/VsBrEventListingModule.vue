6540bd9bc9e2e1dc47b48c0a2c441c2e0d7856bc
Mark Mitchell <mark.mitchell@visitscotland.com>
2025-05-29 10:12:57 +0100

    move featured webinar test outside tabbed section

--- components/Modules/VsBrEventListingModule.vue
+++ components/Modules/VsBrEventListingModule.vue
@@ -1,10 +1,63 @@
 <template>
-    <VsTabs
-        class="my-400 vs-events-listing"
-        :id="moduleId"
-        no-container
-        data-event-listing="True"
-    >
+    <section class="vs-rich-text-wrapper featured">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="vs-heading vs-heading--heading-xl">
+                        Featured webinar
+                    </h2>
+                    <VsEventCard
+                        :cta-label="featuredEvent.cta.label"
+                        :cta-href="featuredEvent.cta.link"
+                        :key="featuredEvent.title"
+                        :data-event-listing="True"
+                    >
+                        <template #event-card-header>
+                            {{ featuredEvent.title }}
+                        </template>
+
+                        <template #event-card-date>
+                            {{ featuredEvent.dates }}
+                        </template>
+
+                        <template #event-card-content>
+                            <VsBrRichText :input-content="featuredEvent.summary" />
+
+                            <VsList unstyled>
+                                <VsRow>
+                                    <VsCol
+                                        cols="2"
+                                        md="4"
+                                    >
+                                        <li>
+                                            <strong>Time:</strong> {{ featuredEvent.times }}
+                                        </li>
+                                        <li>
+                                            <strong>Price:</strong> Free
+                                        </li>
+                                        <li>
+                                            <strong>Location:</strong> Online
+                                        </li>
+                                    </VsCol>
+
+                                    <VsCol
+                                        cols="12"
+                                        md="4"
+                                    >
+                                        <li>
+                                            <strong>Organiser:</strong> VisitScotland
+                                        </li>
+                                    </VsCol>
+                                </VsRow>
+                            </VsList>
+                        </template>
+                    </VsEventCard>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <VsTabs class="my-400 vs-events-listing" :id="moduleId" no-container data-event-listing="True">
         <VsTabItem
             v-for="(eventList, index) in module.eventsListings"
             :key="index"
@@ -12,72 +65,19 @@
         >
             <VsContainer class="mt-300">
                 <VsRow>
-                    <section class="vs-rich-text-wrapper featured">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="vs-heading vs-heading--heading-xl">
-                                        Featured webinar
-                                    </h2>
-                                    <VsEventCard
-                                        :cta-label="featuredEvent.cta.label"
-                                        :cta-href="featuredEvent.cta.link"
-                                        :key="featuredEvent.title"
-                                        :data-event-listing="True"
-                                    >
-                                        <template #event-card-header>
-                                            {{ featuredEvent.title }}
-                                        </template>
-
-                                        <template #event-card-date>
-                                            {{ featuredEvent.dates }}
-                                        </template>
-
-                                        <template #event-card-content>
-                                            <VsBrRichText :input-content="featuredEvent.summary" />
-
-                                            <VsList unstyled>
-                                                <VsRow>
-                                                    <VsCol
-                                                        cols="2"
-                                                        md="4"
-                                                    >
-                                                        <li>
-                                                            <strong>Time: </strong>
-                                                            {{ featuredEvent.times }}
-                                                        </li>
-                                                        <li>
-                                                            <strong>Price:</strong> Free
-                                                        </li>
-                                                        <li>
-                                                            <strong>Location:</strong> Online
-                                                        </li>
-                                                    </VsCol>
-
-                                                    <VsCol
-                                                        cols="12"
-                                                        md="4"
-                                                    >
-                                                        <li>
-                                                            <strong>Organiser: </strong>
-                                                            VisitScotland
-                                                        </li>
-                                                    </VsCol>
-                                                </VsRow>
-                                            </VsList>
-                                        </template>
-                                    </VsEventCard>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <VsHeading level="3" heading-style="heading-m">
+                    <VsHeading
+                        level="3"
+                        heading-style="heading-m"
+                    >
                         {{ eventList.title }}
                     </VsHeading>
 
                     <VsBrRichText :input-content="eventList.copy.value" />
                 </VsRow>
-                <VsBrEventListing :event-data="eventList" :module-id="moduleId" />
+                <VsBrEventListing
+                    :event-data="eventList"
+                    :module-id="moduleId"
+                />
             </VsContainer>
         </VsTabItem>
     </VsTabs>
@@ -141,12 +141,12 @@ const moduleId = computed(() => module.anchor || 'events-listing-module');
 </script>
 
 <style lang="scss">
+
 .featured {
     .vs-event-card {
-        border: 1px solid rgb(233, 233, 233);
+        border: 1px solid rgb(233,233,233);
     }
 }
-
 .vs-tabs--no-container .tab-pane .vs-heading {
     display: block !important;
 
