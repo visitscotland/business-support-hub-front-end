b0080b88c56a7d15c1feaaa0bd45715827be5872
mprosser <Michael.Prosser@visitscotland.com>
2025-09-09 09:14:37 +0100

    fix(civic): switch to interval approach

--- components/Modules/VsBrGtm.vue
+++ components/Modules/VsBrGtm.vue
@@ -39,6 +39,40 @@ if (page && page.isPreview()) {
     isPreviewMode = true;
 }
 
+const attachCivicEvents = (counter = 1) => {
+    if (counter < 20) {
+        if (typeof window !== 'undefined' && window.google_tag_manager) {
+            // GTM can't call browser events directly, so we need to listen for events on the
+            // datalayer and then latch our code onto those.
+            const originalDataLayerPush = window.dataLayer.push;
+
+            window.dataLayer.push = (arg : any) => {
+                if (arg) {
+                    originalDataLayerPush(arg);
+                } else {
+                    originalDataLayerPush();
+                }
+
+                if (arg && arg.event === 'cookie_permission_loaded') {
+                    setTimeout(() => {
+                        window.dispatchEvent(new Event('cookieManagerLoaded'));
+                    });
+                }
+
+                if (arg && arg.event === 'cookie_permission_changed') {
+                    setTimeout(() => {
+                        window.dispatchEvent(new Event('cookiesUpdated'));
+                    });
+                }
+            };
+        } else {
+            setTimeout(() => {
+                attachCivicEvents(counter + 1);
+            }, 500);
+        }
+    }
+};
+
 if (id && !isPreviewMode && import.meta.client) {
     useHead({
         script: [
@@ -49,10 +83,23 @@ if (id && !isPreviewMode && import.meta.client) {
             })(window,document,'script','dataLayer','${id}');`,
         ],
     });
+
+    useHead({
+        script: [
+            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '${queryString}';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${id}');`,
+        ],
+    });
+
+    attachCivicEvents();
 }
 
 if (isPreviewMode && window) {
-    window.bypassCookieChecks = true;
+    window.bypassCookiesRequired = true;
+    window.bypassCookiesLoaded = true;
 }
 
 </script>
