63233c6b73eab2c356c81fdb2e7ffcc677ac45a1
mprosser <Michael.Prosser@visitscotland.com>
2025-09-09 13:31:03 +0100

    fix(civic): make it even more resilient

--- components/Modules/VsBrGtm.vue
+++ components/Modules/VsBrGtm.vue
@@ -39,31 +39,71 @@ if (page && page.isPreview()) {
     isPreviewMode = true;
 }
 
+// To avoid an awkward race condition, we need to check if civic was loaded before we find the
+// datalayer, as well as listening for it being loaded afterwards.
+const checkIfCivicLoaded = () => {
+    for (let x = 0; x < window.dataLayer.length; x++) {
+        let argEvent = '';
+
+        if (window.dataLayer[x].value && window.dataLayer[x].value.event) {
+            argEvent = window.dataLayer[x].value.event;
+        } else if (window.dataLayer[x].event) {
+            argEvent = window.dataLayer[x].event;
+        }
+
+        if (argEvent === 'cookie_permission_loaded') {
+            setTimeout(() => {
+                window.dispatchEvent(new Event('cookieManagerLoaded'));
+            });
+        }
+
+        if (argEvent === 'cookie_permission_changed') {
+            setTimeout(() => {
+                window.dispatchEvent(new Event('cookiesUpdated'));
+            });
+        }
+    }
+};
+
 const attachCivicEvents = (counter = 1) => {
     if (counter < 20) {
         if (typeof window !== 'undefined' && window.google_tag_manager) {
+            checkIfCivicLoaded();
+
             // GTM can't call browser events directly, so we need to listen for events on the
             // datalayer and then latch our code onto those.
             const originalDataLayerPush = window.dataLayer.push;
 
             window.dataLayer.push = (arg : any) => {
+                let res = null;
+
                 if (arg) {
-                    originalDataLayerPush(arg);
+                    res = originalDataLayerPush(arg);
                 } else {
-                    originalDataLayerPush();
+                    return originalDataLayerPush();
                 }
 
-                if (arg && arg.event === 'cookie_permission_loaded') {
+                let argEvent = '';
+
+                if (arg.value && arg.value.event) {
+                    argEvent = arg.value.event;
+                } else if (arg.event) {
+                    argEvent = arg.event;
+                }
+
+                if (argEvent === 'cookie_permission_loaded') {
                     setTimeout(() => {
                         window.dispatchEvent(new Event('cookieManagerLoaded'));
                     });
                 }
 
-                if (arg && arg.event === 'cookie_permission_changed') {
+                if (argEvent === 'cookie_permission_changed') {
                     setTimeout(() => {
                         window.dispatchEvent(new Event('cookiesUpdated'));
                     });
                 }
+
+                return res;
             };
         } else {
             setTimeout(() => {
