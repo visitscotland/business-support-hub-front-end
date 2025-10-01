77c395971c0d3c4583f8948db72ea05b8ecca275
jameshaddrillvs <james.haddrill@visitscotland.com>
2025-10-01 09:55:54 +0100

    feat: add RUM data gathering to site

--- app.vue
+++ app.vue
@@ -49,6 +49,7 @@ import {
     getCurrentInstance, ref, onMounted,
 } from 'vue';
 import mitt from 'mitt';
+import { AwsRum } from 'aws-rum-web';
 
 import VsBrMenu from '~/components/Base/VsBrMenu.vue';
 import VsBrFooter from '~/components/Base/VsBrFooter.vue';
@@ -152,6 +153,35 @@ onMounted(() => {
 
     const hydrationEvent = new Event('vs-app-hydrated');
     window.dispatchEvent(hydrationEvent);
+
+    // implement AWS RUM scripts
+    // note that the identityPoolId, endpoint and APPLICATION_ID values
+    // will be overwritten by the prod build process
+    try {
+        const config = {
+            sessionSampleRate: 1,
+            identityPoolId: runtimeConfig.public.AWS_IDENTITY_POOL_ID,
+            endpoint: runtimeConfig.public.AWS_RUM_ENPOINT,
+            telemetries: ['performance'],
+            allowCookies: false,
+            enableXRay: false,
+        };
+
+        const APPLICATION_ID = runtimeConfig.public.AWS_RUM_APPLICATION_ID;
+        const APPLICATION_VERSION = '1.0.0';
+        const APPLICATION_REGION = 'eu-west-2';
+
+        /* eslint-disable @typescript-eslint/no-unused-vars */ /* eslint-disable no-unused-vars */
+        const awsRum = new AwsRum(
+            APPLICATION_ID,
+            APPLICATION_VERSION,
+            APPLICATION_REGION,
+            config,
+        );
+        /* eslint-enable @typescript-eslint/no-unused-vars */ /* eslint-enable no-unused-vars */
+    } catch (error) {
+        // Ignore errors thrown during CloudWatch RUM web client initialization
+    }
 });
 
 /**
