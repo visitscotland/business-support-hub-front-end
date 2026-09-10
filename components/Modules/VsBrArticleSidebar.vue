447bbd2441fcffb58e7badecbddc1144a98ecb9b
mprosser <Michael.Prosser@visitscotland.com>
2026-09-10 12:47:45 +0100

    Revert "feat(quote): remove quote usage"
    This reverts commit 8f413030cff1374a50a3cd27698eb89509457ac0.

--- components/Modules/VsBrArticleSidebar.vue
+++ components/Modules/VsBrArticleSidebar.vue
@@ -25,6 +25,13 @@
                 <VsBrImageWithCaption :image="section.image.cmsImage" />
             </template>
         </template>
+
+        <template
+            #vs-article-sidebar-quote
+            v-if="section.quote"
+        >
+            <VsBrQuote :quote="section.quote" />
+        </template>
     </VsArticleSidebar>
 </template>
 
@@ -33,6 +40,7 @@ import { VsArticleSidebar } from '@visitscotland/component-library/components';
 
 import useConfigStore from '~/stores/configStore.ts';
 import VsBrImageWithCaption from '~/components/Modules/VsBrImageWithCaption.vue';
+import VsBrQuote from '~/components/Modules/VsBrQuote.vue';
 
 const configStore = useConfigStore();
 
