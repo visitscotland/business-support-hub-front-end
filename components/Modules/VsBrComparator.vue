10fdfa563293acb20def36b2153afef1db04ab28
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2026-09-14 14:51:42 +0100

    fix(vsbrcomparator): missing props and unused function calls

--- components/Modules/VsBrComparator.vue
+++ components/Modules/VsBrComparator.vue
@@ -42,7 +42,6 @@
                                 :value="feature.id"
                                 :label="checkboxLabel(feature.name, feature.description)"
                                 :field-name="feature.id"
-                                @status-update="updateStore"
                             />
                         </div>
                     </fieldset>
@@ -70,7 +69,11 @@
                 lg="7"
                 class="col-xxl-6"
             >
-                <VsHeading level="2" class="mb-200">
+                <VsHeading
+                    class="mb-200"
+                    heading-style="heading-m"
+                    level="2"
+                >
                     {{ labels['results-heading'] }}
                 </VsHeading>
                 <div class="d-flex flex-column gap-200">
@@ -79,7 +82,7 @@
                         v-for="(provider, index) in matchingProviders"
                         :key="provider.name + index"
                     >
-                        <VsHeading level="3">
+                        <VsHeading heading-style="heading-xs" level="3">
                             <VsLink
                                 :href="provider.url"
                                 type="external"
